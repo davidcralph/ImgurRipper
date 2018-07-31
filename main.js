b9ec974ad9ff1cabad9260b19626387d22a83c6c
Dave R <davetheprogrammer2@gmail.com>
2018-07-31 15:25:53 +0100

    fixes and updates

--- main.js
+++ main.js
@@ -10,10 +10,16 @@ if(!fs.existsSync('./rips/')) {
 
 let answer = query('[Ripper] Imgur URL? ');
 if (!answer) {
+    console.log('[Ripper] Error: No URL provided');
+    process.exit();
+}
+if (!answer.includes("imgur.com")) {
+    console.log('[Ripper] Error: Not a valid Imgur URL');
     process.exit();
 }
 let answer2 = query('[Ripper] Folder Name? ');
 if (!answer2) {
+    console.log('[Ripper] Error: No folder name provided');
     process.exit();
 }
     
@@ -21,14 +27,15 @@ if(fs.existsSync(`./rips/${answer2}`)) {
    console.log('[Ripper] Error: This folder already exists');
    let answer3 = query2('[Ripper] Do you want to write to this folder anyway?');
    if (answer3 == false) { 
+       console.log('[Ripper] Error: No answer provided');
        process.exit(); 
  }
 } else {
     console.log('[Ripper] Creating directory');
     fs.mkdirSync(`./rips/${answer2}`);
 }
-console.log('[Ripper] Downloading');
 
+console.log('[Ripper] Downloading');
 exec(`node ./ripper.js ${answer} ./rips/${answer2}`, function(error, stdout, stderr) {
     console.log(`[Ripper] Success: Saved to ./rips/${answer2}`);
 });
