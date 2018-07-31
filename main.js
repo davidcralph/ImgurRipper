fd15f652c3517811c251a4a03b79e72fc48b4ff0
Dave R <davetheprogrammer2@gmail.com>
2018-07-31 11:51:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const fs = require ("fs"),
+    query = require('cli-interact').question,    
+    query2 = require('cli-interact').getYesNo,
+    exec = require('child_process').exec;
+
+if(!fs.existsSync('./rips/')) {
+    fs.mkdirSync('./rips/');
+    console.log('[Ripper] Created /rips/ directory');
+}
+
+let answer = query('[Ripper] Imgur URL? '),
+    answer2 = query('[Ripper] Folder Name? ');
+    
+if(fs.existsSync(`./rips/${answer2}`)) {
+   console.log('[Ripper] Error: This folder already exists');
+   let answer3 = query2('[Ripper] Do you want to write to this folder anyway?');
+   if (answer3 == false) { 
+       process.exit(); 
+ }
+} else {
+    console.log('[Ripper] Creating directory');
+    fs.mkdirSync(`./rips/${answer2}`);
+}
+console.log('[Ripper] Downloading');
+
+exec(`node ./ripper.js ${answer} ./rips/${answer2}`, function(error, stdout, stderr) {
+    console.log(`[Ripper] Success: Saved to ./rips/${answer2}`);
+});
