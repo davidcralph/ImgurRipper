c185c0b3a5a6479ea73ce604af2169152bbb1506
Dave R <davetheprogrammer2@gmail.com>
2018-07-31 16:46:00 +0100

    Update ripper.js

--- ripper.js
+++ ripper.js
@@ -1,27 +1,14 @@
 const request = require('request'),
       fs = require('fs'); 
 
-let clientID = "6b64b0446da80dc";
-
-let unallowChar = "><:\"\\/|?*\0"; 
-
-let args = process.argv.splice(2, 4); 
-
-let customPath = args.length > 1;
-let pathArg
-
-if (customPath) {
-    pathArg = args[1];
-}
-
-let urlImages = "https://api.imgur.com/3/album/" + getAlbumHash(args[0]); 
-
-let optionsImages = {
+let args = process.argv.splice(2, 4),
+    urlImages = "https://api.imgur.com/3/album/" + getAlbumHash(args[0]),
+    optionsImages = {
     url: urlImages,
     headers: {
-        'authorization': 'Client-ID ' + clientID
+        'authorization': 'Client-ID 6b64b0446da80dc'
     }
-}
+ }
 
 function getAlbumHash(url) {
     return url.split("/")[url.split("/").length - 1]
@@ -32,26 +19,18 @@ function parseImages(err, resp, body) {
         process.exit();
     }
 
-    let imgTitles = [];
     let imgLinks = [];
 
     JSON.parse(body, (key, val) => {
-        if (key == "title") {
-            imgTitles.push(val);
-        }
-
         if (key == "link") {
             imgLinks.push(val);
         }
     });
 
-    let dirTitle = pathArg; 
-
     imgLinks = imgLinks.slice(1, imgLinks.length); 
 
     for (image = 0; image < imgLinks.length; image++) {
-        let filename = imgLinks[image].split("/")[3];
-        download(imgLinks[image], filename, dirTitle); 
+        download(imgLinks[image], imgLinks[image].split("/")[3], args[1]); 
     }
 }
 
